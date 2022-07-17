6f8b5847256a9fceef016043a4839bb05f0e1762
Nadya Arassy Primawan <67790978+nadya472@users.noreply.github.com>
2022-07-17 10:11:00 +0700

    Add files via upload

--- modul pasien/function2.php
+++ modul pasien/function2.php
@@ -1,15 +1,6 @@
 <?php
 // koneksi data base
-$host = "localhost";
-$user = "adminklinik";
-$pass = "312010110";
-$db = "klinik_312010110";
-$conn = mysqli_connect($host, $user, $pass, $db);
-if ($conn == false)
-{
-    echo "Koneksi ke server gagal.";
-    die();
-} #else echo "Koneksi berhasil";
+require '../koneksi.php';
 
 function query($query) {
     global $conn;
